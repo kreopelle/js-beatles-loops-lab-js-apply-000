c65fa8291a401c6087e8fbd3808049b00710c3ca
Kayla Reopelle <kreopelle@gmail.com>
2018-02-15 05:19:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(number) {
   var newArray = [];
   do {
-    newArray.push("I love the Beatles!")
-  }
+    newArray.push("I love the Beatles!");
+    number++;
+  } while (number < 15);
 }
