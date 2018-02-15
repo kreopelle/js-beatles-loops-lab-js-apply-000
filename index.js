e475f155758b95863e979562510c36347f0dfdd2
Kayla Reopelle <kreopelle@gmail.com>
2018-02-15 05:17:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function theBeatlesPlay(musicians, instruments) {
+  var newArray = [];
+  for (var i = 0; i < musicians.length; i++) {
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return newArray;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  var newArray = [];
+  while (facts[i]) {
+    newArray.push(facts[i] + "!!!");
+    i++;
+  }
+  return newArray;
+}
+
